6dfa74b884aae07eee9939238acd107e3e0d8b24
Vikas N Kumar <vikas@cpan.org>
2011-05-25 11:24:45 +0000

    NLP: improving unit test. better handling of models as constants.

--- lib/NLP/StanfordParser.pm
+++ lib/NLP/StanfordParser.pm
@@ -9,14 +9,21 @@ use common::sense;
 use Carp;
 
 BEGIN {
-    $NLP::StanfordParser::VERSION = '0.01';
-
+    use Exporter();
+    our @ISA = qw(Exporter);
+    our $VERSION = '0.01';
     # extract the path from the Package
-    my $package = __PACKAGE__;
+    my $package = __PACKAGE__ . '.pm';
     $package =~ s/::/\//g;
-    our $JarPath = $INC{"$package.pm"};
+    our $JarPath = $INC{$package};
     $JarPath =~ s/\.pm$//g;
 }
+use constant {
+    MODEL_FACTORED => "$NLP::StanfordParser::JarPath/englishFactored.ser.gz",
+    MODEL_PCFG => "$NLP::StanfordParser::JarPath/englishPCFG.ser.gz",
+    PARSER_JAR => "$NLP::StanfordParser::JarPath/stanford-parser.jar",
+};
+our @EXPORT = qw(MODEL_FACTORED MODEL_PCFG);
 use Inline (
     Java => << 'END_OF_JAVA_CODE',
 	import java.util.*;
@@ -47,7 +54,7 @@ use Inline (
 		}
 	}
 END_OF_JAVA_CODE
-    CLASSPATH       => "$NLP::StanfordParser::JarPath/stanford-parser.jar",
+    CLASSPATH       => PARSER_JAR,
     EXTRA_JAVA_ARGS => '-Xmx800m'
 );
 use Moose;
@@ -56,14 +63,23 @@ use namespace::autoclean;
 has model => (
     is      => 'ro',
     isa     => 'Str',
-    default => "$NLP::StanfordParser::JarPath/englishPCFG.ser.gz"
+    default => MODEL_PCFG,
 );
-has parser => ( is => 'ro', lazy_build => 1, handles => [qw/parse/] );
+
+has parser => ( is => 'ro', lazy_build => 1, handles => [qw/parse/],
+    isa => 'NLP::StanfordParser::Java',
+);
 
 sub _build_parser {
     my $self = shift;
     return new NLP::StanfordParser::Java( $self->model );
 }
+
+before '_build_parser' => sub {
+    croak 'Unable to find ' . PARSER_JAR unless -e PARSER_JAR;
+    croak 'Unable to find ' . MODEL_PCFG unless -e MODEL_PCFG;
+    croak 'Unable to find ' . MODEL_FACTORED unless -e MODEL_FACTORED;
+};
 
 __PACKAGE__->meta->make_immutable;
 1;
--- t/00nlpstanford.t
+++ t/00nlpstanford.t
@@ -1,21 +1,44 @@
 ### COPYRIGHT: Selective Intellect LLC
 ### AUTHOR: Vikas N Kumar
-### DATE: 8th March 2011
+### DATE: 24th May 2011
 
 use Test::More;
 use Test::Moose;
 
 BEGIN {
-    plan skip_all =>
-'Environment variable NLPSTANFORD should be defined to the directory where the Java libraries reside.'
+    plan skip_all => 'Environment variable NLPSTANFORD should be defined '
+      . 'to the directory where the Java libraries reside.'
       unless defined $ENV{NLPSTANFORD};
     use_ok('NLP::StanfordParser');
 }
-my $nlp = new_ok('NLP::StanfordParser');
-meta_ok($nlp);
-can_ok( $nlp, 'parse' );
 
-my $sentence = 'The quick brown fox jumped over the lazy dog!';
-isnt( $nlp->parse($sentence), undef );
+subtest 'model is PCFG' => sub {
+    my $nlp = new_ok('NLP::StanfordParser');
+    meta_ok($nlp);
+    can_ok( $nlp, 'parse' );
+    has_attribute_ok( $nlp, 'parser' );
+    has_attribute_ok( $nlp, 'model' );
+    is($nlp->model, MODEL_PCFG, 'Model is PCFG');
+    isa_ok( $nlp->parser, 'NLP::StanfordParser::Java' );
+    my $sentence = 'The quick brown fox jumped over the lazy dog!';
+    isnt( $nlp->parse($sentence), undef );
+
+    done_testing();
+};
+
+subtest 'model is FACTORED' => sub {
+    my $nlp = new_ok('NLP::StanfordParser' => [ model => MODEL_FACTORED ]);
+    meta_ok($nlp);
+    can_ok( $nlp, 'parse' );
+    has_attribute_ok( $nlp, 'parser' );
+    has_attribute_ok( $nlp, 'model' );
+    is($nlp->model, MODEL_FACTORED, 'Model is Factored');
+    isa_ok( $nlp->parser, 'NLP::StanfordParser::Java' );
+
+    my $sentence = 'The quick brown fox jumped over the lazy dog!';
+    isnt( $nlp->parse($sentence), undef );
+
+    done_testing();
+};
 
 done_testing();
